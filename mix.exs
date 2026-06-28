ad89769343ef9df92ef605ae651c1b5d513ac630
Lorenzo Sánchez <sanchezfrailelorenzo@gmail.com>
2026-06-28 20:36:00 +0200

    chore(pote): add runtime: false to excoveralls test dep

--- mix.exs
+++ mix.exs
@@ -80,7 +80,7 @@ defmodule Pote.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.18", only: :test},
+      {:excoveralls, "~> 0.18", only: :test, runtime: false},
       {:stream_data, "~> 1.0", only: :test}
     ]
   end
